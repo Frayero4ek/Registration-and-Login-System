6dd154889b4fa03729e5777c8862051e51ba929e
Frayero4ek <rostikcoding@gmail.com>
2024-12-08 18:17:11 +0200

    Збережено поточні зміни

--- src/config.php
+++ src/config.php
@@ -1,7 +1,6 @@
 <?php
-
-const DB_HOST = 'localhost';
+const DB_HOST = 'db';  // Не 'test_db', а ім'я сервісу в Docker Compose
 const DB_PORT = '3306';
-const DB_NAME = 'areaweb';
-const DB_USERNAME = 'root';
-const DB_PASSWORD = '';
+const DB_NAME = 'test_db';  // Перевірте, що база з таким ім'ям існує в MySQL
+const DB_USERNAME = 'admin';
+const DB_PASSWORD = 'admin';
